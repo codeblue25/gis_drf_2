0725c8b92cfdffcf3a4ac068a819a965dc947bc0
codeblue25 <chois1031@gmail.com>
2021-12-13 14:55:22 +0900

    login cookie update

--- accountapp/static/JS/login.js
+++ accountapp/static/JS/login.js
@@ -1,3 +1,29 @@
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+
 function send_input() {
     axios.post('/accounts/login/', {
         username: document.getElementById('username').value,
@@ -11,7 +37,7 @@ function send_input() {
                  += "<div class='btn btn-primary rounded-pill px-5 mb-3'>로그인을 성공했습니다. </div>";
 
         // Token 수령 후 쿠키 생성
-        document.cookie = "drf_token=Token " + response.data['token'];
+        setCookie('drf_token', 'Token ' + response.data['token']);
 
 //        window.location.href = '/accounts/hello_world_template/';
 
