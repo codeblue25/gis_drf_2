8e845e29041f2113a418c5bacfdd02d038f033ca
codeblue25 <chois1031@gmail.com>
2021-12-14 15:57:50 +0900

    Delete template javascript http code update

--- accountapp/static/JS/destroy.js
+++ accountapp/static/JS/destroy.js
@@ -1,19 +1,34 @@
 function delete_account(pk) {
-    axios.delete('/accounts/delete/' + pk)
-      .then(function (response) {
-        // handle success
-        console.log(response);
+    axios({
+        method: 'delete',
+        url: '/accounts/delete/' + pk,
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token')),
+        }
+    })
+        .then(function (response) {
+            // handle success
+            console.log(response);
 
-        window.location.href = '/accounts/hello_world_template/';
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
+            window.location.href = '/accounts/logout/';
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
 
-        document.getElementById('alert_box').innerHTML
-                = '<div class="btn btn-danger rounded-pill px-5">탈퇴를 실패했습니다.</div>'
-      })
-      .then(function () {
-        // always executed
-      });
-};
+            if (error.response.status === 401) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>인증 정보가 없습니다.</div>"
+            } else if (error.response.status === 403) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>권한이 없습니다.</div>"
+            } else {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>탈퇴에 실패했습니다.</div>"
+            }
+
+        })
+        .then(function () {
+            // always executed
+        });
+}
