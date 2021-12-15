f0084ba1a9955be1affc00a1f38f3ead2f05d8b2
codeblue25 <chois1031@gmail.com>
2021-12-15 14:26:10 +0900

    Profile CreateAPIView perform_create method update

--- profileapp/views.py
+++ profileapp/views.py
@@ -12,6 +12,8 @@ from profileapp.serializers import ProfileSerializer
 class ProfileCreateAPIView(CreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
