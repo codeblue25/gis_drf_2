dc06861cd0063bf947c0a30e281838278357ad60
codeblue25 <chois1031@gmail.com>
2021-12-27 09:31:29 +0900

    celery async function error fixed

--- profileapp/tasks.py
+++ profileapp/tasks.py
@@ -34,4 +34,4 @@ def generate_thumbnail_celery_lag(profile_id):
                                                 sys.getsizeof(output),
                                                 None)
 
-    target_profile.save()
+    target_profile.save(async_func=True)
